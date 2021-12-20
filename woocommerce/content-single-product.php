265196a64450f9c490dde312be5c2c0510678877
anisur2805 <anisur2805@gmail.com>
2021-12-20 14:35:56 +0600

    fix single product image show

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,47 +1,47 @@
 <?php
-/**
- * The template for displaying product content in the single-product.php template
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.6.0
- */
+	/**
+	 * The template for displaying product content in the single-product.php template
+	 *
+	 * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
+	 *
+	 * HOWEVER, on occasion WooCommerce will need to update template files and you
+	 * (the theme developer) will need to copy the new files to your theme to
+	 * maintain compatibility. We try to do this as little as possible, but it does
+	 * happen. When this occurs the version of the template file will be bumped and
+	 * the readme will list any important changes.
+	 *
+	 * @see     https://docs.woocommerce.com/document/template-structure/
+	 * @package WooCommerce\Templates
+	 * @version 3.6.0
+	 */
 
-defined( 'ABSPATH' ) || exit;
+	defined( 'ABSPATH' ) || exit;
 
-global $product;
+	global $product;
 
-/**
- * Hook: woocommerce_before_single_product.
- *
- * @hooked woocommerce_output_all_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
+	/**
+	 * Hook: woocommerce_before_single_product.
+	 *
+	 * @hooked woocommerce_output_all_notices - 10
+	 */
+	do_action( 'woocommerce_before_single_product' );
 
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
+	if ( post_password_required() ) {
+		echo get_the_password_form(); // WPCS: XSS ok.
+		return;
+	}
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+<div id="product-<?php the_ID();?>"<?php wc_product_class( '', $product );?>>
 
 	<div class="container single_product_page">
 		<div class="row">
 			<div class="col-md-6">
 					<?php
-					
-						$attachment_ids = $product->get_gallery_image_ids(); 
-					
-						if( !empty($attachment_ids)) {  ?>
-						
+
+						$attachment_ids = $product->get_gallery_image_ids();
+
+					if ( !empty( $attachment_ids ) ) {?>
+
 						<script>
 							jQuery(document).ready(function($){
 								 $('.product-large-image').slick({
@@ -61,69 +61,78 @@ if ( post_password_required() ) {
 								});
 							});
 						</script>
-						
+
 								<div class="product-large-image">
-									<?php 
-										foreach ($attachment_ids as $large_img) {
-											?>
-												
+									<?php
+										foreach ( $attachment_ids as $large_img ) {
+										?>
+
 											<div class="product-large-single-image">
 												<img src="<?php echo wp_get_attachment_image_url( $large_img, 'large' ) ?>" />
-											
+
 											</div>
-											<?php 
-										}
-									
-									?>
+											<?php
+												}
+
+												?>
 								</div>
-								
-								
+
+
 								<div class="product-gallery-image">
-									<?php 
-										foreach ($attachment_ids as $small_img) {
+									<?php
+										foreach ( $attachment_ids as $small_img ) {
 											?>
-												
+
 											<div class="product-gallery-single-image">
 												<img src="<?php echo wp_get_attachment_url( $small_img, 'large' ) ?>" />
-											
+
 											</div>
-											<?php 
-										}
-									
-									?>
+											<?php
+												}
+
+												?>
 								</div>
-								
-								
-							
-							<?php 
-						}
-					
-					
-						/**
-						* Hook: woocommerce_before_single_product_summary.
-						*
-						* @hooked woocommerce_show_product_sale_flash - 10
-						* @hooked woocommerce_show_product_images - 20
-						*/
-						// do_action( 'woocommerce_before_single_product_summary' );
-					?>
+
+
+
+							<?php
+								} else {
+								?>
+							<div class="product-large-image">
+
+											<div class="product-large-single-image">
+													<?php echo woocommerce_get_product_thumbnail( get_the_ID() ); ?>
+											</div>
+
+								</div>
+							<?php
+								}
+
+								/**
+								 * Hook: woocommerce_before_single_product_summary.
+								 *
+								 * @hooked woocommerce_show_product_sale_flash - 10
+								 * @hooked woocommerce_show_product_images - 20
+								 */
+								// do_action( 'woocommerce_before_single_product_summary' );
+							?>
 			</div>
 			<div class="col-md-6">
 					<div class="summary entry-summary">
 						<?php
-						/**
-						* Hook: woocommerce_single_product_summary.
-						*
-						* @hooked woocommerce_template_single_title - 5
-						* @hooked woocommerce_template_single_rating - 10
-						* @hooked woocommerce_template_single_price - 10
-						* @hooked woocommerce_template_single_excerpt - 20
-						* @hooked woocommerce_template_single_add_to_cart - 30
-						* @hooked woocommerce_template_single_meta - 40
-						* @hooked woocommerce_template_single_sharing - 50
-						* @hooked WC_Structured_Data::generate_product_data() - 60
-						*/
-						do_action( 'woocommerce_single_product_summary' );
+							/**
+							 * Hook: woocommerce_single_product_summary.
+							 *
+							 * @hooked woocommerce_template_single_title - 5
+							 * @hooked woocommerce_template_single_rating - 10
+							 * @hooked woocommerce_template_single_price - 10
+							 * @hooked woocommerce_template_single_excerpt - 20
+							 * @hooked woocommerce_template_single_add_to_cart - 30
+							 * @hooked woocommerce_template_single_meta - 40
+							 * @hooked woocommerce_template_single_sharing - 50
+							 * @hooked WC_Structured_Data::generate_product_data() - 60
+							 */
+							do_action( 'woocommerce_single_product_summary' );
 						?>
 					</div>
 			</div>
@@ -135,15 +144,15 @@ if ( post_password_required() ) {
 
 
 	<?php
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
-	do_action( 'woocommerce_after_single_product_summary' );
+		/**
+		 * Hook: woocommerce_after_single_product_summary.
+		 *
+		 * @hooked woocommerce_output_product_data_tabs - 10
+		 * @hooked woocommerce_upsell_display - 15
+		 * @hooked woocommerce_output_related_products - 20
+		 */
+		do_action( 'woocommerce_after_single_product_summary' );
 	?>
 </div>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+<?php do_action( 'woocommerce_after_single_product' );?>
